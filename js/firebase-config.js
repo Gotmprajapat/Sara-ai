4bf89978bea4264fb7290d75b6bd8fa7dd59b266
Gautam <gotmprajapat00@gmail.com>
2026-08-15 13:20:43 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// js/firebase-config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDFuol9Eoa4fItM3ZJcg54WJzD49XCgyZ4",
+  authDomain: "sara-9121f.firebaseapp.com",
+  databaseURL: "https://sara-9121f-default-rtdb.firebaseio.com",
+  projectId: "sara-9121f",
+  storageBucket: "sara-9121f.firebasestorage.app",
+  messagingSenderId: "214013688651",
+  appId: "1:214013688651:web:4c7c3e87ce18b7797006ce",
+  measurementId: "G-98C1LPYQ8Q2"
+};
